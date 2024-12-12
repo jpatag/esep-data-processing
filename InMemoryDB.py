245924b5e0d0eddc97280357cef1461e87a3f3ce
jpatag <112610699+jpatag@users.noreply.github.com>
2024-12-12 01:51:50 +0000

    Added InMemoryDB.py file

---
+++ InMemoryDB.py
@@ -0,0 +1,39 @@
+class InMemoryDB:
+    def __init__(self):
+        self.database = {}
+        self.current = []
+        self.transaction = False
+
+    def get(self, key):
+        print(self.database.get(key, None))
+
+    def put(self, key, val):
+        if self.transaction:
+            self.current.append((key, val))
+        else:
+            raise Exception("Error: Transaction has not yet begun")
+
+    def begin_transaction(self):
+        self.transaction = True
+        self.current = []
+
+    def commit(self):
+        if self.transaction:
+            for key, val in self.current:
+                self.database[key] = val
+        else:
+            raise Exception("Error: Transaction has not yet begun")
+        self.current = []
+        self.transaction = False
+
+    def rollback(self):
+        if self.transaction:
+            self.current = []
+        else:
+            raise Exception("Error: Transaction has not yet begun")
+        self.transaction = False
+
+
+
+
+
